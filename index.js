163d84e25145bf8db73f80f0dc778620fb1b3685
pablofgaeta <pgaeta@ucsc.edu>
2021-06-25 15:59:07 -0700

    convo/msg ui containers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
 const express = require('express');
 const Gun     = require('gun');
+const fetch   = require('node-fetch');
 require('axe');
 
 const app = express();
@@ -8,12 +9,14 @@ app.use(Gun.serve);
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 
+// Register new user. May contain duplicates
 app.post('/signup', function(req, res) {
     const {uname, pass} = req.body;
     const user = gun.user(); 
     user.create(uname, pass, (ack) => res.send(ack));
-})
+});
 
+// Login user.
 app.post('/login', function(req, res) {
     const {uname, pass} = req.body;
     const user = gun.user(); 
@@ -24,10 +27,15 @@ app.post('/login', function(req, res) {
             res.send(ack.put);            
         }
     });
-})
+});
 
 
 const server = app.listen(port);
 const gun = Gun({	file: 'data', web: server });
+
+
 console.log('Server started on port ' + port + ' with /gun');
 console.log(`open link : http://localhost:${port}`);
+
+// const keepAlive = async () => fetch('/');
+// setTimeout(keepAlive, 1500, 'funky');
